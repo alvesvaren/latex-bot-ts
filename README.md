63430075fcfee3d19ef9b61ffe8a6442280a0eff
Alve Svarén <alve@hotmail.se>
2023-01-20 18:59:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Latex bot
+
+Invite: https://discord.com/api/oauth2/authorize?client_id=1066050993403465880&permissions=277025441792&scope=bot
---
+++ global.d.ts
@@ -0,0 +1,14 @@
+declare module 'mathjax' {
+    import * as MathJax from 'mathjax';
+    export function init(options: any): Promise<{
+        tex2svgPromise: (tex: string, options: {display?: boolean}) => Promise<any>;
+    }>;
+}
+
+declare namespace MathJax {
+    const startup: {
+        adaptor: {
+            innerHTML: (node: any) => string;
+        };
+    }
+}
