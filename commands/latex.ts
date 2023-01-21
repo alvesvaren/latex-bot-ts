ccb68e430c2fea294f54c02502fa3188084a0f01
Alve Svarén <alve@hotmail.se>
2023-01-21 02:23:23 +0100

    Revert padding

--- commands/latex.ts
+++ commands/latex.ts
@@ -51,13 +51,6 @@ export const run: CommandRun = async (c, interaction) => {
   const obj = sharp(Buffer.from(svg));
 
   const png = await obj
-    .extend({
-      top: 2,
-      bottom: 2,
-      left: 2,
-      right: 2,
-      background: { r: 255, g: 255, b: 255, alpha: 0 },
-    })
     .resize({
       height: Math.ceil(((await obj.metadata()).height || 16) * scale),
     })
